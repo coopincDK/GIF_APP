e5c0be741b0fb4d71f2741d1a1855610704b51a5
coopincDK <mrm@smartpack.dk>
2026-03-03 21:22:01 +0100

    ok

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+import { AuthProvider } from './context/AuthContext.jsx'
+import { CupModeProvider } from './context/CupModeContext.jsx'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <CupModeProvider>
+          <App />
+          <Toaster
+            position="top-center"
+            toastOptions={{
+              duration: 3000,
+              style: {
+                borderRadius: '16px',
+                fontFamily: 'Nunito, sans-serif',
+                fontWeight: '700',
+              },
+            }}
+          />
+        </CupModeProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
